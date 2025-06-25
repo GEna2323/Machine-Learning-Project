9fa56fb33db2050940edb63e8ab6bcf37caa3652
GEna2323 <bilykgv@gmail.com>
2025-06-25 13:09:36 +0300

    Adding TensorFlow code to the project

--- .venv/app_tf.py
+++ .venv/app_tf.py
@@ -2,13 +2,19 @@ import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import math
 import copy
+import pymysql
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
 import tensorflow as tf
+from db_config import host, user, password, db_name
 from tensorflow.keras import Sequential
-from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
-from tensorflow.keras.layers import Dense, Input, Flatten, Dropout
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.metrics import MeanAbsoluteError, MeanAbsolutePercentageError
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.regularizers import l2
+from sklearn.preprocessing import StandardScaler
+
 
 
 # DataBase
@@ -52,119 +58,61 @@ Y_dev = np.concatenate([Y[30:40], Y[80:90], Y[130:140], Y[180:190]])
 X_test = np.concatenate([X[40:50], X[90:100], X[140:150], X[190:200]])
 Y_test = np.concatenate([Y[40:50], Y[90:100], Y[140:150], Y[190:200]])
 
+# Normalization
+scaler_Y = StandardScaler()
+Y_train_scaled = scaler_Y.fit_transform(Y_train.reshape(-1, 1)).flatten()
+Y_dev_scaled = scaler_Y.transform(Y_dev.reshape(-1, 1)).flatten()
+Y_test_scaled = scaler_Y.transform(Y_test.reshape(-1, 1)).flatten()
 
 
-# model = Sequential([
-#     Dense(3, activation="sigmoid"),
-#     Dense(1, activation="sigmoid")
-# ])
-#
-# predictions = model(X1[:1]).numpy()
-# tf.nn.softmax(predictions).numpy()
-#
-# model.compile(
-#     loss = BinaryCrossentropy,
-#     optimizer = 'adam'
-# )
-# model.fit(
-#     X1, Y1, epochs = 5
-# )
-#
-# model.predict(X_test)
-# model.evaluate(X_test, Y_test, verbose=2)
-
-
-# Load data
-# mnist = tf.keras.datasets.mnist
-#
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# x_train, x_test = x_train / 255.0, x_test / 255.0
-
-
-#Normalizing data
-X_normalized = X.copy()
-for j in range(X.shape[1]):
-    X_normalized[:, j] = (X[:, j] - np.min(X[:, j])) / (np.max(X[:, j]) - np.min(X[:, j]))
-poly = PolynomialFeatures(degree=2, include_bias=False)
-X_poly = poly.fit_transform(X_normalized)
-
-
-# Ініціалізація параметрів
-w_init = np.zeros(X_poly.shape[1])
-b_init = 20000
-
-def tensorflow_model(x, y):
-    #Normalizing data (tensorflow)
+def tensorflow_model(x_train, y_train, x_dev, y_dev, x_test, y_test, lambda_):
+    #Normalizing data
     norm_l = tf.keras.layers.Normalization(axis=-1)
-    norm_l.adapt(x)  # learns mean, variance
-    xn = norm_l(x)
-
+    norm_l.adapt(x_train)
+    x_train_n = norm_l(x_train)
+    x_dev_n = norm_l(x_dev)
+    x_test_n = norm_l(x_test)
 
     tf.random.set_seed(1234)
     model = Sequential(
         [
-            Dense(20, activation='relu', name='layer1'),
-            Dense(4, activation='relu', name='layer2'),
-            Dense(1, activation='relu', name='layer3')
+            Dense(20, activation='relu', name='layer1', kernel_regularizer=l2(lambda_)),
+            Dense(5, activation='relu', name='layer2', kernel_regularizer=l2(lambda_)),
+            Dense(1, name='layer3', kernel_regularizer=l2(lambda_))
         ]
     )
 
-
     model.compile(
-            loss = tf.keras.losses.BinaryCrossentropy(),
-            optimizer = tf.keras.optimizers.Adam(learning_rate=0.01),
+        loss = MeanSquaredError(),
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.01),
+        metrics=[MeanAbsoluteError(), MeanAbsolutePercentageError()]
     )
-
 
     model.fit(
-        xn, y,
-        epochs = 10,
+        x_train_n, y_train,
+        validation_data=(x_dev_n, y_dev),
+        epochs=100,
     )
 
-
     model.summary()
-    return
+    predictions = model.predict(x_test_n)
+    predictions = scaler_Y.inverse_transform(predictions).flatten()
+    y_test_orig = scaler_Y.inverse_transform(y_test.reshape(-1, 1)).flatten()
+    loss, mae, mape = model.evaluate(x_test_n, y_test, verbose=2)
+    return model, predictions, y_test_orig, loss, mae, mape
 
 
 def app():
-    A = 0.001
-    iterations = 100000
-    f_wb = 0.0
     try:
-        choice = int(input("Choose 1(simple regression) or 2(tensorflow) to train model:"))
+        lam = float(input("Print lambda:"))
+        model, predictions, y_test_orig, loss, mae, mape = tensorflow_model(X_train, Y_train_scaled, X_dev, Y_dev_scaled, X_test, Y_test_scaled, lam)
+        print(f"Predictions: {predictions[:5]}")
+        print(f"True Values: {y_test_orig[:5]}")
+        mae_orig = np.mean(np.abs(predictions - y_test_orig))
+        mape_orig = np.mean(np.abs((predictions - y_test_orig) / y_test_orig)) * 100
+        print(f"MAE in original scale: {mae_orig}")
+        print(f"MAPE in original scale: {mape_orig}%")
     except ValueError:
-        print(f"You can write only a number")
-        return
+        return print("Lambda needs to be int or float")
 
-    if choice == 1:
-        reg = input("Activate regularization l2 (y/n):")
-        w_final, b_final, j_history = gradient_descent(X_poly, Y, w_init, b_init, compute_cost, compute_gradient, A, iterations, reg)
-        #App code
-        x_str = input("Введи масив значень: [Пробіг, Витрата палива, Час розгону, Рік, М/А]:")
-        x_test = [float(x) for x in x_str.split(',')]
-        #Normalizing my input data
-        x_test_normalized = np.array(x_test)
-        for j in range(len(x_test_normalized)):
-            x_test_normalized[j] = (x_test[j] - np.min(X[:, j])) / (np.max(X[:, j]) - np.min(X[:, j]))
-        x_test_poly = poly.transform([x_test_normalized])
-        f_wb = np.dot(x_test_poly[0], w_final) + b_final
-        print(f"Передбачена ціна: ${f_wb:.2f}")
-    elif choice == 2:
-        tensorflow_model(X, Y)
-        return
-    else:
-        print(f"There is no variant {choice}, you can only choose 1 or 2!")
-        return
-
-
-    with open("model tests.txt", "r+", encoding='utf-8') as file:
-        file.write(str(f_wb))
-
-    # My plot (cost / iterations)
-    plt.plot(j_history)
-    plt.xlabel('Ітерація')
-    plt.ylabel('Cost')
-    plt.title('Зміна функції втрат для цін автомобілів')
-    plt.yscale('log')
-    plt.show()
 app()
